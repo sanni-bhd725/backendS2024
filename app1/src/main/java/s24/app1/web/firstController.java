448ae29519b8825f37da5086cb06ad947a95219a
sanni-bhd725 <sanni.nevala@myy.haaga-helia.fi>
2024-01-19 20:49:03 +0200

    2. tehtävä lisätty

--- app1/src/main/java/s24/app1/web/firstController.java
+++ app1/src/main/java/s24/app1/web/firstController.java
@@ -2,6 +2,7 @@ package s24.app1.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,6 +18,14 @@ public class firstController {
     @ResponseBody
     public String returnContact() {
         return "This is the contact page";
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String returnGreeting(
+            @RequestParam(value = "location", required = false, defaultValue = "(location value)") String location,
+            @RequestParam(name = "name", required = false, defaultValue = "(name value)") String name) {
+        return "Welcome to the " + location + " " + name + "!";
     }
 
 }

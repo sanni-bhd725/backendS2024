3909cc4702efb8c45a73ea98a6f3fdb5ddd2df01
sanni-bhd725 <149050056+sanni-bhd725@users.noreply.github.com>
2024-01-29 18:04:50 +0200

    Add files via upload

---
+++ Back end Spring 2024/training_project/bookstore/src/main/java/training_project/bookstore/domain/Book.java
@@ -0,0 +1,65 @@
+package training_project.bookstore.domain;
+
+public class Book {
+
+    public String title, author, isbn;
+    public int publicationYear;
+    public double price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn, int publicationYear, double price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn + ", publicationYear=" + publicationYear
+                + ", price=" + price + "]";
+    }
+}
---
+++ Back end Spring 2024/week2/helloThymeleaf/src/main/java/week2/helloThymeleaf/thymeleafApplication.java
@@ -0,0 +1,13 @@
+package week2.helloThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class thymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(thymeleafApplication.class, args);
+	}
+
+}
